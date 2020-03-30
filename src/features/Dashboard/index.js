59487726342446418a83d40c04dd991e3805852c
dustin0116 <dustin0116@gmail.com>
2020-03-30 15:01:08 -0700

    Removed unnecessary code

--- src/features/Dashboard/index.js
+++ src/features/Dashboard/index.js
@@ -5,36 +5,13 @@ import Colonies from 'components/Colonies';
 // import Cart from 'components/Cart';
 
 const Dashboard = () => {
-  // const { getCart } = useProfileProvider();
-  // const { state, state: { name: { first } } } = useProfileProvider();
-  // const [showCart, setShowCart] = useState(false);
-  // const [cartToggle, setCartToggle] = useState(false);
-
-  // const setClose = () => {
-  //   setCartToggle(false);
-  //   setShowCart(false);
-  // };
-
-  // const setShow = () => {
-  //   setCartToggle(true);
-  //   setShowCart(true);
-  //   getCart();
-  // };
 
   return (
     <div className="dashboard" style={{ textAlign: 'center' }}>
-      <div style={{textAlign: 'left' }}>
+      <div style={{textAlign: 'left'}}>
         <Logout />
       </div>
       <Colonies />
-
-      {/* {cartToggle ? <Button
-        variant="outlined"
-        onClick={setClose}
-      >
-                        Add Item
-                    </Button> : <Button variant="outlined" onClick={setShow}>Show Cart</Button>}
-      {showCart ? <Cart /> : <ItemForm />} */}
     </div>
   );
 };
