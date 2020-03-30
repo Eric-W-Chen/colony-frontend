8c814c48d40ada9f38096ecb31eba24319dc3ceb
dustin0116 <dustin0116@gmail.com>
2020-03-30 15:01:19 -0700

    Update format

--- src/features/Registration/index.js
+++ src/features/Registration/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Register from 'components/RegistrationForm';
 
-const Registration = () => (
-  <div className="registration">
-    <h1 style={{ textAlign: 'center' }}>Register for an account.</h1>
-    <Register />
-  </div>
-);
+const Registration = () => {
+
+  return (
+    <div className="registration">
+      <Register />
+    </div>
+  );
+};
 
 export default Registration;
