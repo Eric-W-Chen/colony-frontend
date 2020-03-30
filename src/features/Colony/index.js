a56fbcff53cf85f523c968b97a8cbdab25c707b9
dustin0116 <dustin0116@gmail.com>
2020-03-29 18:02:21 -0700

    Added single colony component

---
+++ src/features/Colony/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useProfileProvider } from 'contexts/profile';
+import { Button } from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
+import Animals from 'components/Animals';
+
+
+const Colony = () => {
+  const [redirectToColonies, setRedirectToColonies] = useState(false);
+
+  if (redirectToColonies) {
+    return <Redirect to="/dashboard" />;
+  }
+  
+  return (
+    <div className="animals">
+      <Button onClick={() => setRedirectToColonies(true)}>Back to colonies</Button>
+      <Animals /> 
+    </div>
+  );
+};
+
+export default Colony;
